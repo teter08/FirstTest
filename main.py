77b198a6ed2485b9a3a245561be366c72a9b39d2
teter <ia>
2022-04-20 17:55:36 +0300

    zad6

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print('Hello, World, from Russia')
-print('devel')
+def useless(lst):
+    print(len(max(lst, key=lambda x:len(x))))
+
+    return lst
+
+print(useless(['111','22222','33sdfdfsd33','44']))
