e038caf767ec3c27405031af6b1264e69c024538
teter <ia>
2022-06-07 13:09:58 +0300

    indi 2 chap

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-from second import *
-
-print(sec)
+a,b,c = input().split()
+print('Simvol code',a,'is',str(ord(a))+'.')
+print('Simvol code',b,'is',str(ord(b))+'.')
+print('Simvol code',c,'is',str(ord(c))+'.')
