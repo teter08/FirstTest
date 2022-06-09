ea96c35f1655572b9748b25c89e4b30c4ac794cf
teter08 <72251892+teter08@users.noreply.github.com>
2022-06-09 22:15:36 +0300

    ({}[]) - do it

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 s = input()
 stek = []
+bool=True
 if len(s) % 2 == 0:
     for i in range(len(s)):
         if s[i] == '(' or s[i] == '[' or s[i] == '{':
             stek.append(s[i])
-        elif (stek[-1] == '(' and s[i] == ')') or (stek[-1] == '[' and s[i] == ']') or (
-                stek[-1] == '{' and s[i] == '}'):
-            stek = stek[:-1]
+        elif stek:
+            if (stek[-1] == '(' and s[i] == ')') or (stek[-1] == '[' and s[i] == ']') or (
+                    stek[-1] == '{' and s[i] == '}'):
+                stek = stek[:-1]
+            else:
+                bool=False
+                break
+        else:
+            bool=False
+            break
 else:
-    print('NO')
-print('YES' if s else 'NO')
+    bool=False
+print('YES' if s and bool else 'NO')
