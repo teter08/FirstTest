017062b99388467d4b541d24873556805227d4f5
teter <ia>
2022-06-09 17:54:11 +0300

    (){}[]

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-n = int(input())
-i = 1
-while i <= n//2:
-    if n % i == 0:
-        print(i, end=' ')
-    i+=1
-print(n)
+s = input()
+stek = []
+if len(s) % 2 == 0:
+    for i in range(len(s)):
+        if s[i] == '(' or s[i] == '[' or s[i] == '{':
+            stek.append(s[i])
+        elif (stek[-1] == '(' and s[i] == ')') or (stek[-1] == '[' and s[i] == ']') or (
+                stek[-1] == '{' and s[i] == '}'):
+            stek = stek[:-1]
+else:
+    print('NO')
+print('YES' if s else 'NO')
--- second.py
+++ second.py
@@ -4,3 +4,11 @@ a = input().lower()
 b = input().lower()
 
 a = int(input())
+mystr = list(map(int, input().split()))
+a, b = map(int, input().split())
+a1, b1 = a, b
+while b > 0:
+    c = a % b
+    a = b
+    b = c
+print((a1*b1)//a)
