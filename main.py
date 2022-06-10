b63c28aa40a0f96b7e0cc5d66df43c3181d45b07
teter <ia>
2022-06-10 16:38:17 +0300

    list

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-s = input()
-s = list(map(int, s))
-count = [0] * 10
-for i in s:
-    count[i] += 1
-for i, v in enumerate(count):
-    if v != 0: print(i, v)
+# n, m = map(int, input().split())
+n, m = 3, 4
+a = []
+x=0
+for i in range(n):
+    if i%2!=0:
+        a.append([x for x in range(m)])
+
+print(a)
--- second.py
+++ second.py
@@ -1,14 +1,23 @@
 a, b, c = map(int, input().split())
 
-a = input().lower()
-b = input().lower()
-
-a = int(input())
 mystr = list(map(int, input().split()))
+
+n = int(input())
+for i in range(n):
+    a.append(list(map(int, input().split())))
+
+for i in range(4):
+    a.append(input())
+
 a, b = map(int, input().split())
 a1, b1 = a, b
 while b > 0:
     c = a % b
     a = b
     b = c
-print((a1*b1)//a)
+print((a1 * b1) // a)
+
+3 3
+6 2 7
+1 2 8
+1 3 8
