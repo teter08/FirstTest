239b5569fb538b6e31abf7c712edab077919cf6d
teter <ivanbarashkov@yandex.ru>
2022-07-14 17:15:54 +0300

    class

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-# Создайте кортеж из 7-ми именованных кортежей учащихся ВУЗов. В именованном кортеже будут присутствовать
-# следующие поля: имя студента, возраст, оценка за семестр, город проживания. Функция good_students()
-# будет принимать этот кортеж, вычислять среднюю оценку по всем учащимся и выводить на печать следующее
-# сообщение: “Ученики {список имен студентов через запятую} в этом семестре хорошо учатся!”.
-# В список студентов, которые выводятся по результатам работы функции, попадут лишь те, у которых оценка за
-# семестр равна или выше средней по всем учащимся.
+class Graph:
+    def __init__(self, data: list, is_show=True):
+        self.data = data
+        self.is_show = is_show
 
-from collections import namedtuple
+    def set_data(self, data):
+        self.data = data
 
-lst = ['Name', 'Age', 'Marks', 'City']
-Student = namedtuple('Student', lst)
-students = (
-    Student('stud1', 20, 1, 'Yaros'),
-    Student('stud2', 21, 2, 'Yaros'),
-    Student('stud3', 22, 3, 'Yaros'),
-    Student('stud4', 23, 4, 'Yaros'),
-    Student('stud5', 24, 5, 'Yaros'),
-    Student('stud6', 25, 5, 'Yaros'),
-    Student('stud7', 36, 5, 'Yaros'))
+    def show_table(self):
+        if self.is_show:
+            print(*self.data)
+        else:
+            print('Отображение данных закрыто')
+
+    def show_graph(self):
+        if self.is_show:
+            print('Графическое отображение данных:', *self.data)
+        else:
+            print('Отображение данных закрыто')
+
+    def show_bar(self):
+        if self.is_show:
+            print('Столбчатая диаграмма:', *self.data)
+        else:
+            print('Отображение данных закрыто')
+
+    def set_show(self, fl_show):
+        self.is_show = fl_show
 
 
-def good_students(students):
-    summarks = 0
-    for student in students:
-        summarks += student.Marks
-    return summarks / len(students)
+# data_graph = list(map(int, input().split()))
+data_graph = [8, 11, 10, -32, 0, 7, 18]
+gr_1 = Graph(data_graph)
+gr_1.show_bar()
+gr_1.set_show(False)
+gr_1.show_table()
 
-
-print(good_students(students))
