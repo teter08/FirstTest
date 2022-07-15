66288cd02e68d95492a5f56a79d303bd5b9d189a
teter <ivanbarashkov@yandex.ru>
2022-07-15 16:38:39 +0300

    1.5.10

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ class Cell:
         self.around_mines = around_mines
         self.mine = mine
         self.fl_open = False
+
+
+class GamePole:
+    def __init__(self, n: int, m: int):
+        self.n = n
+        self.m = m
+        self.pole = []
+
+    def init(self):
+        pass
+
+    def show(self):
+        pass
