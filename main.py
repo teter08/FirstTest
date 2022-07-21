02589798c0e77ee520fc695229062642da396609
teter <ivanbarashkov@ya.ru>
2022-07-21 22:56:43 +0300

    1.8

--- main.py
+++ main.py
@@ -6,15 +6,28 @@ class Server:
         self.buffer = buffer
         Server.idip += 1
 
+    def send_data(self, data, ip):
+        pass
+
+    def get_data(self) -> list:
+        pass
+
+    def get_ip(self) -> int:
+        return self.ip
+
 
 class Router:
     servs = {}
+    buffer = []
 
     def link(self, server):
         self.servs[id(server)] = server
 
     def unlink(self, server):
         self.servs.pop(id(server))
+
+    def send_data(self):
+        pass
 
 
 class Data:
@@ -23,13 +36,18 @@ class Data:
         self.ip = ip
 
 
-s1 = Server()
-s2 = Server()
-s3 = Server()
-s4 = Server()
-r1 = Router()
-r1.link(s1)
-r1.link(s2)
-r1.link(s3)
-r1.link(s4)
-r1.unlink(s1)
+router = Router()
+sv_from = Server()
+sv_from2 = Server()
+router.link(sv_from)
+router.link(sv_from2)
+router.link(Server())
+router.link(Server())
+sv_to = Server()
+router.link(sv_to)
+sv_from.send_data(Data("Hello", sv_to.get_ip()))
+sv_from2.send_data(Data("Hello", sv_to.get_ip()))
+sv_to.send_data(Data("Hi", sv_from.get_ip()))
+router.send_data()
+msg_lst_from = sv_from.get_data()
+msg_lst_to = sv_to.get_data()
