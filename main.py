5e243ed0c1603989bf83eac8547e436ebc077b00
teter <ivanbarashkov@yandex.ru>
2022-07-22 16:11:07 +0300

    1.8

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 class Server:
     idip = 1
 
-    def __init__(self, buffer=None):
+    def __init__(self):
         self.ip = Server.idip
-        self.buffer = buffer
+        self.buffer = []
+        self.myrouter = None
         Server.idip += 1
 
-    def send_data(self, data, ip):
-        pass
+    def send_data(self, data):
+        if self.myrouter:
+            self.myrouter.buffer.append(data)
 
     def get_data(self) -> list:
-        pass
+        b = self.buffer[:]
+        self.buffer.clear()
+        return b
 
     def get_ip(self) -> int:
         return self.ip
 
 
 class Router:
-    servs = {}
-    buffer = []
+    def __init__(self):
+        self.servs = {}
+        self.buffer = []
 
     def link(self, server):
-        self.servs[id(server)] = server
+        self.servs[server.ip] = server
+        server.myrouter = self
 
     def unlink(self, server):
-        self.servs.pop(id(server))
+        s = self.servs.pop(server.ip)
+        if s:
+            s.myrouter = None
 
     def send_data(self):
-        pass
+        for b in self.buffer:
+            if b.ip in self.servs:
+                self.servs[b.ip].buffer.append(b)
+        self.buffer.clear()
 
 
 class Data:
@@ -37,17 +48,18 @@ class Data:
 
 
 router = Router()
-sv_from = Server()
-sv_from2 = Server()
-router.link(sv_from)
-router.link(sv_from2)
+s1 = Server()
+s2 = Server()
+router.link(s1)
+router.link(s2)
 router.link(Server())
 router.link(Server())
-sv_to = Server()
-router.link(sv_to)
-sv_from.send_data(Data("Hello", sv_to.get_ip()))
-sv_from2.send_data(Data("Hello", sv_to.get_ip()))
-sv_to.send_data(Data("Hi", sv_from.get_ip()))
+s3 = Server()
+router.link(s3)
+s1.send_data(Data("Hello", s3.get_ip()))
+s2.send_data(Data("Hello", s3.get_ip()))
+s3.send_data(Data("Hi", s1.get_ip()))
 router.send_data()
-msg_lst_from = sv_from.get_data()
-msg_lst_to = sv_to.get_data()
+msg_lst_from = s1.get_data()
+msg_lst_to = s3.get_data()
+print(msg_lst_to)
