628c015aec4fbb69b8e9a783cf29913425a897be
Barashkov <ivanbarashkov@yandex.ru>
2023-02-21 17:30:11 +0300

    OOP 2.1

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-class Config:
-    pass
-
-
-def create_instance(n: int) -> Config:
-    obj = Config()
-    [setattr(obj,f'attribute{i+1}', f"value{i+1}") for i in range(n)]
-    return obj
-
-
-
-create_instance(3)
-print(create_instance(3).__dict__)
+class Constructor:
+    def add_atribute(self):
