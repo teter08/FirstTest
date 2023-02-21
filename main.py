0ce6262a5deb3032d4a54c65bb449275f89b070e
Barashkov <ivanbarashkov@yandex.ru>
2023-02-21 15:57:41 +0300

    OOP 1.3

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-class Person:
-    name = "John Smith"
-    age = 30
-    gender = "male"
-    address = "123 Main St"
-    phone_number = "555-555-5555"
-    email = "johnsmith@example.com"
-    is_employed = True
+class Config:
+    pass
 
 
-l = list(input().split())
-print(*[f'{i}-YES' if i.lower() in Person.__dict__ else f'{i}-NO' for i in l], sep='\n')
+def create_instance(n: int) -> Config:
+    obj = Config()
+    [setattr(obj,f'attribute{i+1}', f"value{i+1}") for i in range(n)]
+    return obj
+
+
+
+create_instance(3)
+print(create_instance(3).__dict__)
