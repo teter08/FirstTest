2f0b09c9e847b50351a1337c7bebe693f15fec28
Barashkov <ivanbarashkov@yandex.ru>
2023-02-22 14:45:23 +0300

    OOP 2.1.12

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 class Constructor:
-    def add_atribute(self):
+    def add_atribute(self, attr, value):
+        setattr(self, attr, value)
+
+    def display(self):
+        print(self.__dict__)
+
+
+obj1 = Constructor()
+obj1.display()  # печатает {}
+obj1.add_atribute('color', 'red')
+obj1.add_atribute('width', 20)
+obj1.display()  # печатает {'color': 'red', 'width': 20}
+
+obj2 = Constructor()
+obj2.display()  # печатает {}
+obj2.add_atribute('height', 100)
+obj2.display()  # печатает {'height': 100}
