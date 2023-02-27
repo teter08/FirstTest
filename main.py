85db25c486ee07a498d1774c524406b1f59b4715
Barashkov <ivanbarashkov@yandex.ru>
2023-02-27 17:51:58 +0300

    OOP 2.3.6

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
-class Person:
-    def __init__(self, first_name, last_name, age):
-        self.first_name, self.last_name, self.age = first_name, last_name, age
+class Stack:
+    def __init__(self):
+        self.values = []
 
-    def full_name(self) -> str:
-        return f'{self.last_name} {self.first_name}'
+    def push(self, item):
+        self.values.append(item)
 
-    def is_adult(self) -> bool:
-        return self.age >= 18
+    def pop(self):
+        try:
+            return self.values.pop()
+        except:
+            print('Empty Stack')
+
+    def peek(self):
+        try:
+            return self.values[-1]
+        except:
+            print('Empty Stack')
+            return None
+
+    def is_empty(self):
+        return len(self.values) == 0
+
+    def size(self):
+        return len(self.values)
 
 
-p1 = Person('Jimi', 'Hendrix', 18)
-print(p1.full_name())  # выводит "Hendrix Jimi"
-print(p1.is_adult()) # выводит "True"
+s = Stack()
+s.peek()  # распечатает 'Empty Stack'
+print(s.is_empty())  # распечатает True
+s.push('cat')  # кладем элемент 'cat' на вершину стека
+s.push('dog')  # кладем элемент 'dog' на вершину стека
+print(s.peek())  # распечатает 'dog'
+s.push(True)  # кладем элемент True на вершину стека
+print(s.size())  # распечатает 3
+print(s.is_empty())  # распечатает False
+s.push(777)  # кладем элемент 777 на вершину стека
+print(s.pop())  # удаляем элемент 777 с вершины стека и печатаем его
+print(s.pop())  # удаляем элемент True с вершины стека и печатаем его
+print(s.size())  # распечатает 2
