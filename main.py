63af851898348a5d6892152472761c31be624630
teter <ivanbarashkov@ya.ru>
2023-02-28 22:56:20 +0300

    OOP 2.7.3

--- main.py
+++ main.py
@@ -1,42 +1,11 @@
-class UserMail:
-    def __init__(self, login, email):
-        self.login, self.__email = login, email
+class Notebook:
+    def __init__(self, notes: list):
+        self._notes = notes
 
-    def get_email(self):
-        return self.__email
-
-    def set_email(self, newmail: str):
-        try:
-            if newmail.count('@') == 1 and newmail.index('@') < newmail.index('.'):
-                self.__email = newmail
-            else:
-                print(f'ErrorMail:{newmail}')
-        except:
-            print(f'ErrorMail:{newmail}')
-
-    email = property(fget=get_email, fset=set_email)
+    @property
+    def notes_list(self):
+        [print(f'{i}.{value}', sep='\n') for i, value in enumerate(self._notes, start=1)]
 
 
-jim = UserMail("aka47", 'hello@com.org')
-assert jim.login == "aka47"
-assert jim._UserMail__email == "hello@com.org"
-assert isinstance(jim, UserMail)
-assert isinstance(type(jim).email, property), 'Вы не создали property email'
-
-jim.email = [1, 2, 3]  # печатает ErrorMail:[1, 2, 3]
-jim.email = 'hello@@re.ee'  # печатает ErrorMail:hello@@re.ee
-jim.email = 'hello@re.w3'
-assert jim.email == 'hello@re.w3'
-
-k = UserMail('belosnezhka', 'prince@wait.you')
-assert k.email == 'prince@wait.you'
-assert k.login == 'belosnezhka'
-assert isinstance(k, UserMail)
-
-k.email = {1, 2, 3}  # печатает ErrorMail:{1, 2, 3}
-k.email = 'prince@still@.wait'  # печатает ErrorMail:prince@still@.wait
-k.email = 'prince@stillwait'  # печатает ErrorMail:prince@stillwait
-k.email = 'prince@still.wait'
-assert k.get_email() == 'prince@still.wait'
-k.email = 'pri.nce@stillwait'  # печатает ErrorMail:pri.nce@stillwait
-assert k.email == 'prince@still.wait'
+note = Notebook(['Buy Potato', 'Buy Carrot', 'Wash car'])
+note.notes_list
