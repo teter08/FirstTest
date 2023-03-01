705c771c37fdd17c4a62df976e9b099b665706c5
Barashkov <ivanbarashkov@yandex.ru>
2023-03-01 17:49:41 +0300

    OOP 2.3.11 not

--- main.py
+++ main.py
@@ -1,44 +1,79 @@
-class Money:
-    def __init__(self, dollars, cents):
-        self.total_cents = dollars * 100 + cents
+class Task:
+    def __init__(self, name, description, status=False):
+        self.name, self.description, self.status = name, description, status
 
-    @property
-    def dollars(self):
-        return int(self.total_cents // 100)
-
-    @dollars.setter
-    def dollars(self, value):
-        try:
-            if value >= 0 and isinstance(value, int):
-                self.total_cents = value * 100 + self.total_cents % 100
-            else:
-                print('Error dollars')
-        except:
-            print('Error dollars')
-
-    @property
-    def cents(self):
-        return int(self.total_cents % 100)
-
-    @cents.setter
-    def cents(self, value):
-        try:
-            if 0 <= value < 100 and isinstance(value, int):
-                self.total_cents = (self.total_cents // 100) * 100 + value
-            else:
-                print('Error cents')
-        except:
-            print('Error cents')
-
-    def __str__(self):
-        return f'Ваше состояние составляет {self.dollars} долларов {self.cents} центов'
+    def display(self):
+        print(f'{self.name} ' + ['Сделана', 'Не сделана'][self.status])
 
 
-Bill = Money(101, 99)
-print(Bill)  # Ваше состояние составляет 101 долларов 99 центов
-print(Bill.dollars, Bill.cents)  # 101 99
-print(Bill.total_cents) # 10199
-Bill.dollars = 666
-print(Bill)  # Ваше состояние составляет 666 долларов 99 центов
-Bill.cents = 12
-print(Bill)  # Ваше состояние составляет 666 долларов 12 центов
+class TaskList:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, value):
+        self.tasks.append(value)
+
+    def remove_task(self, value):
+        self.tasks.remove(value)
+
+
+class TaskManager:
+    def __init__(self, newtask: TaskList):
+        self.task_list = newtask
+
+    def mark_done(self, truetask: Task):
+        truetask.status = True
+
+    def mark_undone(self, falsetask: Task):
+        falsetask.status = False
+
+    def show_tasks(self):
+        [print(a.display()) for a in self.task_list.tasks]
+
+
+# Создаем список задач
+todo = TaskList()
+assert todo.tasks == []
+
+# Создаем несколько задач и добавляем их в список
+task1 = Task("Стирка", "Постирать трусы, носки, слюнявчики")
+assert task1.name == 'Стирка'
+assert task1.description == 'Постирать трусы, носки, слюнявчики'
+assert task1.status is False
+task1.display()
+
+todo.add_task(task1)
+assert len(todo.tasks) == 1
+
+task2 = Task("Продукты", "Купить лук чеснок огурцы хлеб и биток")
+assert task2.name == 'Продукты'
+assert task2.description == 'Купить лук чеснок огурцы хлеб и биток'
+assert task2.status is False
+
+todo.add_task(task2)
+assert len(todo.tasks) == 2
+
+# Создаем менеджер задач и показываем список задач
+manager = TaskManager(todo)
+assert isinstance(manager.task_list, TaskList)
+print('-----Список дел-----')
+manager.show_tasks()
+
+# Отмечаем первую задачу выполненной и показываем список задач
+manager.mark_done(task1)
+
+# Проверяем изменился ли статус 2мя способами
+assert task1.status is True
+assert manager.task_list.tasks[0].status is True
+
+print('-----Список дел-----')
+manager.show_tasks()
+
+# Удаляем вторую задачу и показываем список задач
+todo.remove_task(task2)
+
+assert len(todo.tasks) == 1
+assert len(manager.task_list.tasks) == 1
+
+print('-----Список дел-----')
+manager.show_tasks()
