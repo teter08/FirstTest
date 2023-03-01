f0a4efa821cff92fbe9cd2d3c6e20e95c69f8f50
Barashkov <ivanbarashkov@yandex.ru>
2023-03-01 10:24:17 +0300

    OOP 2.7.7 декоратор prorerty, свойства геттеров и сеттеров

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
-class Notebook:
-    def __init__(self, notes: list):
-        self._notes = notes
+class Money:
+    def __init__(self, dollars, cents):
+        self.total_cents = dollars * 100 + cents
 
     @property
-    def notes_list(self):
-        [print(f'{i}.{value}', sep='\n') for i, value in enumerate(self._notes, start=1)]
+    def dollars(self):
+        return int(self.total_cents // 100)
+
+    @dollars.setter
+    def dollars(self, value):
+        try:
+            if value >= 0 and isinstance(value, int):
+                self.total_cents = value * 100 + self.total_cents % 100
+            else:
+                print('Error dollars')
+        except:
+            print('Error dollars')
+
+    @property
+    def cents(self):
+        return int(self.total_cents % 100)
+
+    @cents.setter
+    def cents(self, value):
+        try:
+            if 0 <= value < 100 and isinstance(value, int):
+                self.total_cents = (self.total_cents // 100) * 100 + value
+            else:
+                print('Error cents')
+        except:
+            print('Error cents')
+
+    def __str__(self):
+        return f'Ваше состояние составляет {self.dollars} долларов {self.cents} центов'
 
 
-note = Notebook(['Buy Potato', 'Buy Carrot', 'Wash car'])
-note.notes_list
+Bill = Money(101, 99)
+print(Bill)  # Ваше состояние составляет 101 долларов 99 центов
+print(Bill.dollars, Bill.cents)  # 101 99
+print(Bill.total_cents) # 10199
+Bill.dollars = 666
+print(Bill)  # Ваше состояние составляет 666 долларов 99 центов
+Bill.cents = 12
+print(Bill)  # Ваше состояние составляет 666 долларов 12 центов
