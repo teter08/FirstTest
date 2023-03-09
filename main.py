811d225a89abd210895ee4a42b5df1b14f805b6c
Barashkov <ivanbarashkov@yandex.ru>
2023-03-09 17:24:54 +0300

    OOP 2.11.8 2/3

--- main.py
+++ main.py
@@ -1,57 +1,41 @@
-from string import ascii_letters
+import collections
 
 
-class Registration:
-    def __init__(self, newlogin, newpassword):
-        self.login = newlogin
-        self.password = newpassword
+class Product:
+    def __init__(self, name, price):
+        self.name, self.price = name, price
 
-    @property
-    def password(self):
-        return self.__password
 
-    @password.setter
-    def password(self, newpassword):
-        if not isinstance(newpassword, str): raise TypeError("Пароль должен быть строкой")
-        if not (5 <= len(newpassword) < 12): raise ValueError('Пароль должен быть длиннее 4 и меньше 12 символов')
-        if not Registration.is_include_digit(newpassword): raise ValueError(
-            'Пароль должен содержать хотя бы одну цифру')
-        if not Registration.is_include_all_register(newpassword): raise ValueError(
-            'Пароль должен содержать хотя бы один символ верхнего и нижнего регистра')
-        if not Registration.is_include_only_latin(newpassword): raise ValueError(
-            'Пароль должен содержать только латинский алфавит')
-        if Registration.check_password_dictionary(newpassword): raise ValueError(
-            'Ваш пароль содержится в списке самых легких')
+class User:
+    def __init__(self, login, balance=0):
+        self.login = login
+        self.balance = balance
 
-    @staticmethod
-    def is_include_digit(newpassword):
-        return 1 in [1 if a.isdigit() else 0 for a in newpassword]
-
-    @staticmethod
-    def is_include_all_register(newpassword):
-        return newpassword != newpassword.lower() and newpassword != newpassword.upper()
-
-    @staticmethod
-    def is_include_only_latin(newpassword):
-        return not 0 in [1 if a in ascii_letters else 0 for a in newpassword if not a.isdigit()]
-
-    @staticmethod
-    def check_password_dictionary(newpassword):
-        with open('easy_passwords.txt') as file:
-            lines = [line.rstrip() for line in file]
-        return newpassword in lines
+    def __str__(self):
+        return f'Пользователь {self.login}, баланс - {self.balance}'
 
     @property
-    def login(self):
-        return self.__login
+    def balance(self):
+        return self.__balance
 
-    @login.setter
-    def login(self, newlogin):
-        if not isinstance(newlogin, str): raise TypeError
-        if newlogin.count('@') != 1: raise ValueError
-        if not newlogin.count('.', newlogin.find('@')) > 0: raise ValueError
-        self.__login = newlogin
+    @balance.setter
+    def balance(self, newbalance):
+        self.__balance = newbalance
+
+    def deposit(self, newdeposit):
+        self.__balance += newdeposit
+
+    def payment(self, newpayment):
+        if self.__balance - newpayment >= 0:
+            self.__balance = self.__balance - newpayment
+            return True
+        else:
+            print('Не хватает средств на балансе. Пополните счет')
+            return False
 
 
-a = Registration('1@dfghj.kl2', 'QwerT123')
-# print(Registration.is_include_only_latin('dgdfgFFF'))
+class Cart:
+    def __init__(self, user):
+        self.user = user
+        self.goods = collections.defaultdict()
+        self.__total = 0
