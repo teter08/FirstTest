4dd8b0f670f9968c58da237119fce103298c670f
teter <ivanbarashkov@ya.ru>
2023-03-13 22:26:50 +0300

    OOP 3.2.7

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-class GroceryItem:
-    def __init__(self, name, price, quantity):
-        self.name, self.price, self.quantity = name, price, quantity
+class Hero:
+    def __len__(self):
+        return len(self.__dict__)
 
     def __str__(self):
-        return f'Name: {self.name}\nPrice: {self.price}\nQuantity: {self.quantity}'
-
-    def __repr__(self):
-        return f'GroceryItem({self.name}, {self.price}, {self.quantity})'
+        return '\n'.join([f'{a}: {b}' for a, b in sorted(self.__dict__.items())])
 
 
-banana = GroceryItem('Banana', 10, 5)
-print(banana)
-print(f"{banana!r}")
+hero = Hero()
+assert len(hero) == 0
+hero.health = 50
+hero.damage = 10
+assert len(hero) == 2
+assert str(hero) == '''damage: 10
+health: 50'''
+hero.weapon = ['sword', ' bow']
+hero.skill = 'Некромант'
+assert str(hero) == '''damage: 10
+health: 50
+skill: Некромант
+weapon: ['sword', ' bow']'''
+print(hero)
 
-dragon_fruit = GroceryItem('Dragon fruit', 5, 350)
-print(dragon_fruit)
-print(f"{dragon_fruit!r}")
+villain = Hero()
+assert str(villain) == ''
+assert len(villain) == 0
+villain.level = 15
+villain.skill = 'Боец'
+villain.armor = 25
+assert len(villain) == 3
+assert str(villain) == '''armor: 25
+level: 15
+skill: Боец'''
+print(villain)
