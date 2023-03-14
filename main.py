e2572a99dfedd0a1b29a954e16d1ffc85e3e6554
teter <ivanbarashkov@ya.ru>
2023-03-14 23:22:02 +0300

    OOP 3.3.9

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-class Hero:
-    def __len__(self):
-        return len(self.__dict__)
+class Order:
+    def __init__(self, cart: list, customer: str):
+        self.cart, self.customer = cart, customer
 
-    def __str__(self):
-        return '\n'.join([f'{a}: {b}' for a, b in sorted(self.__dict__.items())])
+    def __add__(self, other):
+        return Order(self.cart + [other], self.customer)
+
+    def __radd__(self, other):
+        return Order([other] + self.cart, self.customer)
+
+    def __sub__(self, other):
+        try:
+            self.cart.remove(other)
+        except:
+            pass
+        return Order(self.cart, self.customer)
+
+    def __rsub__(self, other):
+        return self - other
 
 
-hero = Hero()
-assert len(hero) == 0
-hero.health = 50
-hero.damage = 10
-assert len(hero) == 2
-assert str(hero) == '''damage: 10
-health: 50'''
-hero.weapon = ['sword', ' bow']
-hero.skill = 'Некромант'
-assert str(hero) == '''damage: 10
-health: 50
-skill: Некромант
-weapon: ['sword', ' bow']'''
-print(hero)
+order = Order(['banana', 'apple'], 'Гена Букин')
 
-villain = Hero()
-assert str(villain) == ''
-assert len(villain) == 0
-villain.level = 15
-villain.skill = 'Боец'
-villain.armor = 25
-assert len(villain) == 3
-assert str(villain) == '''armor: 25
-level: 15
-skill: Боец'''
-print(villain)
+order_2 = order + 'orange'
+assert order.cart == ['banana', 'apple']
+assert order.customer == 'Гена Букин'
+assert order_2.cart == ['banana', 'apple', 'orange']
+
+order = 'mango' + order
+assert order.cart == ['mango', 'banana', 'apple']
+order = 'ice cream' + order
+assert order.cart == ['ice cream', 'mango', 'banana', 'apple']
+
+order = order - 'banana'
+assert order.cart == ['ice cream', 'mango', 'apple']
+
+order3 = order - 'banana'
+assert order3.cart == ['ice cream', 'mango', 'apple']
+
+order = order - 'mango'
+assert order.cart == ['ice cream', 'apple']
+order = 'lime' - order
+assert order.cart == ['ice cream', 'apple']
+print('Good')
