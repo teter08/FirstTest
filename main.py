87eec60851416137012c30c1a7827787ceb7c0d1
teter <ivanbarashkov@ya.ru>
2023-03-21 22:50:18 +0300

    OOP 3.9.14

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-class Card:
-    def __init__(self, rank, suit):
-        self.rank = rank
-        self.suit = suit
-
-
-class Deck:
-    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-    pos = -1
-
+class InfinityIterator:
     def __init__(self):
-        self.cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+        self.index = -10
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.pos < len(self.cards)-1:
-            self.pos += 1
-            return f'{self.cards[self.pos].rank} {self.cards[self.pos].suit}'
-        else:
-            raise StopIteration
+        self.index += 10
+        return self.index
 
 
-deck = Deck()
-for card in deck:
-    print(card)
+a = iter(InfinityIterator())
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
