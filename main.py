d6e1e91b50b30363006795fe9b1e5dbdb39efe2f
Barashkov <ivanbarashkov@yandex.ru>
2023-03-23 17:39:42 +0300

    OOP 4.5.5

--- main.py
+++ main.py
@@ -1,28 +1,61 @@
-class Card:
-    def __init__(self, rank, suit):
-        self.rank = rank
-        self.suit = suit
+class Transport:
+    def __init__(self, brand, max_speed, kind=None):
+        self.brand, self.max_speed, self.kind = brand, max_speed, kind
+
+    def __str__(self):
+        return f'Тип транспорта {self.kind} марки {self.brand} может развить скорость {self.max_speed} км/ч'
 
 
-class Deck:
-    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-    pos = -1
+class Car(Transport):
+    def __init__(self, brand, max_speed, mileage, gasoline_residue):
+        super().__init__(brand, max_speed, 'Car')
+        self.__gasoline_residue = gasoline_residue
+        self.mileage = mileage
 
-    def __init__(self):
-        self.cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+    @property
+    def gasoline(self):
+        return f'Осталось бензина на {self.__gasoline_residue} км'
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.pos < len(self.cards)-1:
-            self.pos += 1
-            return f'{self.cards[self.pos].rank} {self.cards[self.pos].suit}'
+    @gasoline.setter
+    def gasoline(self, value):
+        if type(value) == int:
+            self.__gasoline_residue += value
+            print(f'Объем топлива увеличен на {value} л и составляет {self.__gasoline_residue} л')
         else:
-            raise StopIteration
+            print('Ошибка заправки автомобиля')
 
 
-deck = Deck()
-for card in deck:
-    print(card)
+class Boat(Transport):
+    def __init__(self, brand, max_speed, owners_name):
+        super().__init__(brand, max_speed, 'Boat')
+        self.owners_name = owners_name
+
+    def __str__(self):
+        return f'Этой лодкой марки {self.brand} владеет {self.owners_name}'
+
+
+class Plane(Transport):
+    def __init__(self, brand, max_speed, capacity):
+        super().__init__(brand, max_speed, 'Plane')
+        self.capacity = capacity
+
+    def __str__(self):
+        return f'Самолет марки {self.brand} вмещает в себя {self.capacity} людей'
+
+
+transport = Transport('Telega', 10)
+print(transport)  # Тип транспорта None марки Telega может развить скорость 10 км/ч
+bike = Transport('shkolnik', 20, 'bike')
+print(bike)  # Тип транспорта bike марки shkolnik может развить скорость 20 км/ч
+
+first_plane = Plane('Virgin Atlantic', 700, 450)
+print(first_plane)  # Самолет марки Virgin Atlantic вмещает в себя 450 людей
+first_car = Car('BMW', 230, 75000, 300)
+print(first_car)  # Тип транспорта Car марки BMW может развить скорость 230 км/ч
+print(first_car.gasoline)  # Осталось бензина на 300 км
+first_car.gasoline = 20  # Печатает 'Объем топлива увеличен на 20 л и составляет 320 л'
+print(first_car.gasoline)  # Осталось бензина на 320 км
+second_car = Car('Audi', 230, 70000, 130)
+second_car.gasoline = [None]  # Печатает 'Ошибка заправки автомобиля'
+first_boat = Boat('Yamaha', 40, 'Petr')
+print(first_boat)  # Этой лодкой марки Yamaha владеет Petr
