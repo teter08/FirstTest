1308c3e27221b5d327fd825088638a9fce287ac9
Barashkov <ivanbarashkov@yandex.ru>
2023-03-24 16:00:50 +0300

    OOP 4.6.3

--- main.py
+++ main.py
@@ -1,61 +1,24 @@
-class Transport:
-    def __init__(self, brand, max_speed, kind=None):
-        self.brand, self.max_speed, self.kind = brand, max_speed, kind
+class Person:
+    def __init__(self, name, age):
+        self.name, self.age = name, age
 
-    def __str__(self):
-        return f'Тип транспорта {self.kind} марки {self.brand} может развить скорость {self.max_speed} км/ч'
-
-
-class Car(Transport):
-    def __init__(self, brand, max_speed, mileage, gasoline_residue):
-        super().__init__(brand, max_speed, 'Car')
-        self.__gasoline_residue = gasoline_residue
-        self.mileage = mileage
-
-    @property
-    def gasoline(self):
-        return f'Осталось бензина на {self.__gasoline_residue} км'
-
-    @gasoline.setter
-    def gasoline(self, value):
-        if type(value) == int:
-            self.__gasoline_residue += value
-            print(f'Объем топлива увеличен на {value} л и составляет {self.__gasoline_residue} л')
-        else:
-            print('Ошибка заправки автомобиля')
+    def display_person_info(self):
+        print(f'Person: {self.name}, {self.age}')
 
 
-class Boat(Transport):
-    def __init__(self, brand, max_speed, owners_name):
-        super().__init__(brand, max_speed, 'Boat')
-        self.owners_name = owners_name
+class Company:
+    def __init__(self, company_name, location):
+        self.company_name, self.location = company_name, location
 
-    def __str__(self):
-        return f'Этой лодкой марки {self.brand} владеет {self.owners_name}'
-
-
-class Plane(Transport):
-    def __init__(self, brand, max_speed, capacity):
-        super().__init__(brand, max_speed, 'Plane')
-        self.capacity = capacity
-
-    def __str__(self):
-        return f'Самолет марки {self.brand} вмещает в себя {self.capacity} людей'
+    def display_company_info(self):
+        print(f'Company: {self.company_name}, {self.location}')
 
 
-transport = Transport('Telega', 10)
-print(transport)  # Тип транспорта None марки Telega может развить скорость 10 км/ч
-bike = Transport('shkolnik', 20, 'bike')
-print(bike)  # Тип транспорта bike марки shkolnik может развить скорость 20 км/ч
+class Employee(Person, Company):
+    def __init__(self, name, age, company_name, location):
+        super().__init__(name, age)
+        Company.__init__(self, company_name, location)
 
-first_plane = Plane('Virgin Atlantic', 700, 450)
-print(first_plane)  # Самолет марки Virgin Atlantic вмещает в себя 450 людей
-first_car = Car('BMW', 230, 75000, 300)
-print(first_car)  # Тип транспорта Car марки BMW может развить скорость 230 км/ч
-print(first_car.gasoline)  # Осталось бензина на 300 км
-first_car.gasoline = 20  # Печатает 'Объем топлива увеличен на 20 л и составляет 320 л'
-print(first_car.gasoline)  # Осталось бензина на 320 км
-second_car = Car('Audi', 230, 70000, 130)
-second_car.gasoline = [None]  # Печатает 'Ошибка заправки автомобиля'
-first_boat = Boat('Yamaha', 40, 'Petr')
-print(first_boat)  # Этой лодкой марки Yamaha владеет Petr
+emp = Employee('Jessica', 28, 'Google', 'Atlanta')
+emp.display_person_info()
+emp.display_company_info()
