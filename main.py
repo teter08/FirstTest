b41d80d24c7c8ddeb9483344a429c7dc29faeb1e
Barashkov <ivanbarashkov@yandex.ru>
2023-04-14 16:39:31 +0300

    OOP 5.5.8

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
-def func():
-    print('Оно живое')
+class PasswordInvalidError(Exception):
+    pass
 
 
-class CustomButton:
-    def __init__(self, text, **kwargs):
-        self.text = text
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-    def config(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-    def click(self):
-        try:
-            self.command()
-        except AttributeError:
-            print('Кнопка не настроена')
-        except TypeError:
-            print('Кнопка сломалась')
+class PasswordLengthError(PasswordInvalidError):
+    pass
 
 
-btn = CustomButton(text="Hello", bd=20, bg='#ffaaaa')
-btn.click()  # Кнопка не настроена
-btn.config(command=func)
-btn.click()  # Оно живое
+class PasswordContainUpperError(PasswordInvalidError):
+    pass
+
+
+class PasswordContainDigitError(PasswordInvalidError):
+    pass
+
+
+class User:
+    def __init__(self, username, password=None):
+        self.username, self.password = username, password
+
+    def set_password(self, newpassword):
+        if len(newpassword) < 8:
+            raise PasswordLengthError('Пароль должен быть не менее 8 символов')
+        if not any(a.isupper() for a in newpassword):
+            raise PasswordContainUpperError('Пароль должен содержать хотя бы одну заглавную букву')
+        if not any(a.isdigit() for a in newpassword):
+            raise PasswordContainDigitError('Пароль должен содержать хотя бы одну цифру')
+        self.password = newpassword
+
+
+assert issubclass(PasswordInvalidError, Exception)
+assert issubclass(PasswordLengthError, PasswordInvalidError)
+assert issubclass(PasswordContainUpperError, PasswordInvalidError)
+assert issubclass(PasswordContainDigitError, PasswordInvalidError)
+
+user = User("johndoe")
+
+try:
+    user.set_password("weakpwd")
+except PasswordLengthError as e:
+    print(e)
+
+try:
+    user.set_password("strongpassword8")
+except PasswordContainUpperError as e:
+    print(e)
+
+try:
+    user.set_password("Safepassword")
+except PasswordContainDigitError as e:
+    print(e)
+
+user.set_password("SecurePass123")
+assert user.password == 'SecurePass123'
